ca5e5e1e8f87f20ce4f9353e02153931b2c0ad09
MCassimus <morgan.cassimus@gmail.com>
2018-07-16 19:42:53 -0600

    R File

---
+++ Super_Heroes.R
@@ -0,0 +1,11 @@
+
+
+
+setwd("/Users/MorganSwaney/Desktop/Practicum 2")
+
+powers <- read.csv("super_hero_powers.csv", header = T)
+str(powers)
+
+info <- read.csv("heroes_information.csv", header = T)
+str(info)
+
